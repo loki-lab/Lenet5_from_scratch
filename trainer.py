994fe9ef39e2ec77f9a471eafcf031502a5386b0
Loke <nguyenvanminhcuong2002@gmail.com>
2024-01-21 01:21:19 +0700

    commit

---
+++ trainer.py
@@ -0,0 +1,56 @@
+import torch
+from tqdm import tqdm
+
+
+class Trainer:
+    def __init__(self, model, criterion, optimizer, device):
+        self.model = model
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.device = device
+
+    def fit(self, train_loader, valid_loader, max_epoch):
+        self.model = self.model.to(self.device)
+        self.model.train()
+
+        for epoch in range(max_epoch):
+            print('Epoch: {}/{}.. '.format(epoch + 1, max_epoch))
+            running_loss = 0.0
+            running_corrects = 0.0
+            val_loss = 0.0
+            val_corrects = 0.0
+
+            for data, target in tqdm(train_loader):
+                data, target = data.to(self.device), target.to(self.device)
+
+                output = self.model(data)
+                loss = self.criterion(output, target)
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+                _, predicted = torch.max(output.data, 1)
+
+                running_loss += loss.item()
+                running_corrects += predicted.eq(target).sum().item()
+
+            epoch_loss = running_loss / len(train_loader.dataset)
+            epoch_acc = running_corrects / len(train_loader.dataset)
+
+            print('Loss: {:.4f}, Accuracy: {:.4f}'.format(epoch_loss, epoch_acc))
+
+            with torch.no_grad():
+                self.model.eval()
+                for data, target in valid_loader:
+                    data, target = data.to(self.device), target.to(self.device)
+
+                    output = self.model(data)
+                    val_loss = self.criterion(output, target)
+
+                    _, predicted = torch.max(output.data, 1)
+                    val_corrects += predicted.eq(target).sum().item()
+
+                val_loss = val_loss / len(valid_loader.dataset)
+                val_acc = val_corrects / len(valid_loader)
+                print('Validation Loss: {:.4f}, Validation Accuracy: {:.4f}'.format(val_loss, val_acc))
---
+++ training.py
@@ -0,0 +1,34 @@
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torch import nn
+from model import LeNet5
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+from trainer import Trainer
+import torch
+
+train_data_path = "datasets/Training"
+validation_data_path = "datasets/Testing"
+num_classes = 4
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("device:", device)
+
+transforms = transforms.Compose([transforms.ToTensor(),
+                                 transforms.Resize((512, 512), antialias=True),
+                                 transforms.Normalize((0.5,), (0.5,)),
+                                 ])
+
+
+train_dataset = ImageFolder(train_data_path, transform=transforms)
+val_dataset = ImageFolder(validation_data_path, transform=transforms)
+
+train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
+
+model = LeNet5(num_classes=4)
+criterion = nn.CrossEntropyLoss()
+optimizer = Adam(model.parameters(), lr=0.001)
+
+trainer = Trainer(model, criterion, optimizer, device)
+trainer.fit(train_loader, val_loader, 20)
