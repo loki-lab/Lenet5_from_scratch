48821440d997651d0dd8793a1423054c990df610
Loke <nguyenvanminhcuong2002@gmail.com>
2024-01-21 02:13:48 +0700

    commit

--- training.py
+++ training.py
@@ -14,22 +14,25 @@ num_classes = 4
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("device:", device)
 
-transforms = transforms.Compose([transforms.ToTensor(),
-                                 transforms.Resize((512, 512), antialias=True),
-                                 transforms.Normalize((0.5,), (0.5,)),
-                                 transforms.RandomPerspective(distortion_scale=0.5, p=0.1),
-                                 transforms.RandomRotation(degrees=(0, 180)),
-                                 transforms.RandomHorizontalFlip(p=0.5),
-                                 transforms.RandomVerticalFlip(p=0.5),
-                                 transforms.RandomAffine(degrees=(30, 70),
-                                                         translate=(0.1, 0.3),
-                                                         scale=(0.5, 0.75)),
-                                 transforms.RandomInvert()
-                                 ])
+transforms = {"training": transforms.Compose([transforms.ToTensor(),
+                                              transforms.Resize((512, 512), antialias=True),
+                                              transforms.Normalize((0.5,), (0.5,)),
+                                              transforms.RandomPerspective(distortion_scale=0.5, p=0.1),
+                                              transforms.RandomRotation(degrees=(0, 180)),
+                                              transforms.RandomHorizontalFlip(p=0.5),
+                                              transforms.RandomVerticalFlip(p=0.5),
+                                              transforms.RandomAffine(degrees=(30, 70),
+                                                                      translate=(0.1, 0.3),
+                                                                      scale=(0.5, 0.75)),
+                                              transforms.RandomInvert()
+                                              ]),
+              "validation": transforms.Compose([transforms.Resize((512, 512), antialias=True),
+                                                transforms.ToTensor(),
+                                                transforms.Normalize((0.5,), (0.5,))])
+              }
 
-
-train_dataset = ImageFolder(train_data_path, transform=transforms)
-val_dataset = ImageFolder(validation_data_path, transform=transforms)
+train_dataset = ImageFolder(train_data_path, transform=transforms["training"])
+val_dataset = ImageFolder(validation_data_path, transform=transforms["validation"])
 
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
