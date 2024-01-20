91b2b6b136f60a3934a4a97d214a28ee622248c0
Loke <nguyenvanminhcuong2002@gmail.com>
2024-01-21 01:59:54 +0700

    commit

--- trainer.py
+++ trainer.py
@@ -8,6 +8,7 @@ class Trainer:
         self.criterion = criterion
         self.optimizer = optimizer
         self.device = device
+        self.best_metric = 0.0
 
     def fit(self, train_loader, valid_loader, max_epoch):
         self.model = self.model.to(self.device)
@@ -39,6 +40,12 @@ class Trainer:
             epoch_acc = running_corrects / len(train_loader.dataset)
 
             print('Loss: {:.4f}, Accuracy: {:.4f}'.format(epoch_loss, epoch_acc))
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': self.model.state_dict(),
+                'optimizer_state_dict': self.optimizer.state_dict(),
+                'loss': epoch_loss,
+            }, "checkpoint/latest_checkpoint.pth")
 
             with torch.no_grad():
                 self.model.eval()
@@ -54,3 +61,12 @@ class Trainer:
                 val_loss = val_loss / len(valid_loader.dataset)
                 val_acc = val_corrects / len(valid_loader.dataset)
                 print('Validation Loss: {:.4f}, Validation Accuracy: {:.4f}'.format(val_loss, val_acc))
+                if val_acc > self.best_metric:
+                    self.best_metric = val_acc
+                    torch.save({
+                        'epoch': epoch,
+                        'model_state_dict': self.model.state_dict(),
+                        'optimizer_state_dict': self.optimizer.state_dict(),
+                        'loss': epoch_loss,
+                    }, "checkpoint/best_checkpoint.pth")
+
